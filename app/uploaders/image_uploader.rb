8defe871a853c07c50e90ed906e48079941d7073
naoki-nakajima <vsme@i.softbank.jpgit config --global user.name naoki-nakajima>
2021-02-19 22:46:40 +0900

    posts#indexページの修正。メインページ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   version :medium do
-    process resize_to_fill: [1080, 1080]
+    process resize_to_fill: [500, 500]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
