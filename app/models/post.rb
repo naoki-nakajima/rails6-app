056775b36b915fa5def4b967ce83944cda5195f1
naoki-nakajima <vsme@i.softbank.jpgit config --global user.name naoki-nakajima>
2021-03-05 13:09:20 +0900

    トップページの投稿時のビュー崩れの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :foods, inverse_of: :post, dependent: :destroy
-  has_many :post_hashtag_relations
+  has_many :post_hashtag_relations, dependent: :destroy
   has_many :hashtags, through: :post_hashtag_relations
   
   accepts_nested_attributes_for :photos
